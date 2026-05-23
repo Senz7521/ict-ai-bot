ee264f367032bc28770cf76fcebd6226caee4398
Senz7521 <tsr0997@gmail.comecho>
2026-05-23 22:58:46 +0530

    fix

--- trade_bot.py
+++ trade_bot.py
@@ -1,12 +1,24 @@
-import ccxt
+ import ccxt
 import pandas as pd
 import time
 
-exchange = ccxt.bybit({
-    "apiKey": "n40XJzeSxB2mttbbBE",
-    "secret": "******",
-    "enableRateLimit": True
+def new_func():
+    exchange = ccxt.bybit({
+    "apiKey": "MJFbQ47VjMvvApEBG0",
+    "secret": "KWqVRuKV4p1BLFYA21EVhEjCVkmu33ht6N28",
+    "enableRateLimit": True,
 })
+    
+    return exchange
+
+exchange = new_func()
+
+# DEMO MODE
+exchange.set_sandbox_mode(True)
+
+ticker = exchange.fetch_ticker("BTC/USDT")
+
+print(ticker)
 
 SYMBOL = "BTC/USDT"
 TIMEFRAME = "1m"

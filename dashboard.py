b3e98b0693a60459ffa2363c5fdbcd23e2d5f9d7
Senz7521 <tsr0997@gmail.comecho>
2026-05-23 08:41:29 +0530

    final fix

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ st.title("ICT AI BOT")
 # =========================
 # EXCHANGE
 # =========================
-exchange = ccxt.coinbase()
+exchange = ccxt.coinbase({
     'enableRateLimit': True,
     'options': {
         'defaultType': 'future'
